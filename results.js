1e164447d4a474c5d37f248f65c4435dfb69dfd4
Mahmoud Hassan <38259921+mahmoudh-web@users.noreply.github.com>
2023-04-09 21:09:37 +0100

    Update results.js

--- results.js
+++ results.js
@@ -1,3 +1,4 @@
+import { sortBy, max } from "lodash-es"
 import { database } from "./lib/db.js"
 import getInstruments from "./lib/getInstruments.js"
 
@@ -9,21 +10,39 @@ const instruments = await getInstruments()
 const collection = db.collection("v4_results")
 await client.connect()
 
-// get results for each instrument
-for await (let instrument of instruments) {
-	const pointer = collection
-		.find({ instrument: instrument })
-		.sort({ profit: -1 })
-		.limit(1)
+const pointer = collection.find({})
+// .sort({ profit: -1 })
+// .limit(1)
 
-	const results = []
-	await pointer.forEach(res => results.push(res))
-	if (results.length) {
-		console.log(`${instrument}: $${results[0].profit.toFixed(2)}`)
-		// console.log(JSON.stringify(results[0].fast_macd, null, 2))
-		// console.log(JSON.stringify(results[0].slow_macd, null, 2))
-		// console.log(JSON.stringify(results[0].stoch, null, 2))
+const results = []
+await pointer.forEach(res => results.push(res))
+
+// get results for each instrument
+let good = 0
+const goodTokens = []
+let profit = 0
+for await (let instrument of instruments) {
+	const data = results.filter(r => r.instrument === instrument)
+	if (data.length) {
+		const profits = data.map(d => d.profit)
+		const highest = max(profits)
+		if (highest >= 10) {
+			good++
+			profit += highest
+			goodTokens.push(instrument)
+		}
+		console.log(`${instrument}: $${highest.toFixed(2)}`)
 	}
+	// console.log(JSON.stringify(results[0].fast_macd, null, 2))
+	// console.log(JSON.stringify(results[0].slow_macd, null, 2))
+	// console.log(JSON.stringify(results[0].stoch, null, 2))
 }
+
+console.log(`\nProfit above $10: ${good}`)
+console.log(
+	`\nTotal Profit: $${profit.toFixed(2)}, Capital Needed: $${good * 15}`
+)
+console.log(`Return: ${((profit / (good * 15)) * 100).toFixed(2)}%`)
+console.log(goodTokens)
 
 await client.close()
